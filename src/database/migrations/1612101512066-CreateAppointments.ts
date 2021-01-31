e9b9f5a93d2a1eb1440644443eb979d55e720f24
Pablo Ribeiro <69174054+pablobr2100@users.noreply.github.com>
2021-01-31 12:33:44 -0300

    Relaciona model e Configura operações do banco de dados
    Faz a relação entre o model e a tabela do banco de dados usando entity.
    Usa os conceitos de repositório para fazer operações no banco de dados. A rota e o service foram modificados para funcionar junto com o repositório.

--- src/database/migrations/1612101512066-CreateAppointments.ts
+++ src/database/migrations/1612101512066-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1612101512066
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
