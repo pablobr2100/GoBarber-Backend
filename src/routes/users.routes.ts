c6de7537ea52341401a07fa3e77984c05c97dea1
Pablo Ribeiro <69174054+pablobr2100@users.noreply.github.com>
2021-02-03 11:14:47 -0300

    Implementa Upload
    Implementa a opção de upload na aplicação para que possa ser feito o upload do avatar do usuário.
    Salva o arquivo para que possa ser usado depois.
    Cria rota para que estes arquivos possam ser usados pelo front-end.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -29,5 +35,33 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json(userWithoutPassword);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  },
+);
 
 export default usersRouter;
